30df832483824e26d2c161d5c517060e411073d9
Beatriz <biastephanyreis@hotmail.com>
2024-01-23 22:24:06 -0300

    Adicionadas pages js de navegação site

---
+++ pages/ferramentas.js
@@ -0,0 +1,73 @@
+import React from 'react'
+
+const Ferramentas = () => {
+  
+  return (
+    <>
+
+    { <main className="container">
+    <div className=" ">
+      {/* flex-column flex-sm-row justify-content-center  align-items-center */}
+      <ul className="row d-nav list-unstyled justify-content-center align-items-center">
+        {/* d-nav list-unstyled d-flex flex-wrap gap-4 justify-content-center  gap-3 */}
+        <li className="col-12 col-md-6 col-lg-3">
+          <a href="#" className="btn btn-primary d-block mb-3 me-md-3">
+            Simulador de empréstimo
+          </a>
+        </li>
+        <li className="col-12 col-md-6 col-lg-3 ">
+          <a href="#" className="btn btn-primary d-block mb-3 me-md-3">
+            Gestão financeira
+          </a>
+        </li>
+        <li className="col-12 col-md-6 col-lg-3 ">
+          <a href="#" className="btn btn-primary d-block mb-3 me-md-3">
+            Gestão de estoque
+          </a>
+        </li>
+        <li className="col-12 col-md-6 col-lg-3 ">
+          <a href="#" className="btn btn-primary d-block mb-3 me-md-3">
+            Gestão de pessoas
+          </a>
+        </li>
+      </ul>
+    </div>
+    <section>
+      <h3>Nossos softwares</h3>
+      <p>
+        Lorem ipsum dolor sit amet. Qui autem temporibus ut rerum quaerat At
+        deleniti nemo sed dicta inventore. Aut illo illo sed illum quia quo
+        magnam illum. Vel tenetur iusto ut voluptatem molestiae vel voluptas
+        obcaecati est fugit quisquam est asperiores autem ut tempora sint hic
+        autem sequi. Est cumque delectus qui odit quae eos necessitatibus sint
+        qui velit officiis et dolorem ipsum ut omnis eius. Ut officia fugiat aut
+        dolores nemo qui iste velit. Id ducimus internos hic commodi amet ab
+        officiis iste id omnis rerum sit dolorem debitis aut molestias nulla! Et
+        repellendus illo et galisum quibusdam qui dolore ipsam et temporibus
+        sunt est sunt nisi. Sed quidem quibusdam eum galisum quasi et omnis
+        repellat.
+      </p>
+    </section>
+    <section>
+      <h3>Nossas Planilhas</h3>
+      <p>
+        Lorem ipsum dolor sit amet. Qui autem temporibus ut rerum quaerat At
+        deleniti nemo sed dicta inventore. Aut illo illo sed illum quia quo
+        magnam illum. Vel tenetur iusto ut voluptatem molestiae vel voluptas
+        obcaecati est fugit quisquam est asperiores autem ut tempora sint hic
+        autem sequi. Est cumque delectus qui odit quae eos necessitatibus sint
+        qui velit officiis et dolorem ipsum ut omnis eius. Ut officia fugiat aut
+        dolores nemo qui iste velit. Id ducimus internos hic commodi amet ab
+        officiis iste id omnis rerum sit dolorem debitis aut molestias nulla! Et
+        repellendus illo et galisum quibusdam qui dolore ipsam et temporibus
+        sunt est sunt nisi. Sed quidem quibusdam eum galisum quasi et omnis
+        repellat.
+      </p>
+    </section>
+  </main>}
+     ferramentas
+    </>
+  )
+}
+
+export default Ferramentas
