c0ed8a518f02edf2a417de40ca61a877a17f63bd
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-01-24 20:38:25 -0300

    Update create.js

--- pages/admin/empreendedores/create.js
+++ pages/admin/empreendedores/create.js
@@ -1,219 +1,107 @@
-import "bootstrap/dist/css/bootstrap.css";
-import axios from "axios";
-import Link from "next/link";
 import { useState } from "react";
-import { useRouter } from "next/router";
-import style from "@/styles/Home.module.css";
+import FormCreateUpdateDelete from "@/components/dashboard/FormCreateUpdateDelete";
+import { createEmpreendedor } from "@/apiCalls/empreendedor";
 
-const AddEmpreendedor = () => {
-  const [newEmpreendedor, setNewEmpreendedor] = useState({
-    nome: "",
-    sobrenome: "",
-    email: "",
-    telefone: "",
-    dataNascimento: "",
-    nomeEmpresa: "",
-    cidade: "",
-    estado: "",
-    bairro: "",
-    cep: "",
-  });
-  const router = useRouter();
+const initialFormData = {
+  nome: "",
+  sobrenome: "",
+  email: "",
+  telefone: "",
+  dataNascimento: "",
+  nomeEmpresa: "",
+  cidade: "",
+  estado: "",
+  bairro: "",
+  cep: "",
+};
+
+const CreateEmpreendedor = () => {
+  const [newEmpreendedor, setNewEmpreendedor] = useState(initialFormData);
 
   const handleInputChange = (e) => {
     setNewEmpreendedor({ ...newEmpreendedor, [e.target.name]: e.target.value });
   };
 
-  const handleAddEmpreendedor = () => {
-    axios
-      .post("http://localhost:8080/api/empreendedores", newEmpreendedor)
-      .then(() => {
-        router.push("/admin/empreendedores");
+  const handleFormSubmit = () => {
+    // chama a função de criação da API passando os dados do formulário
+    createEmpreendedor(newEmpreendedor)
+      .then((response) => {
+        console.log("Empreendedor criado com sucesso:", response);
       })
       .catch((error) => {
-        alert("Erro ao inserir empreendedor:" + error);
+        console.error("Erro ao criar empreendedor:", error);
       });
   };
 
   return (
-    <>
-      <div className="mx-auto">
-        <h1 className={style.h1}>Inserir Empreendedor</h1>
-
-        <form className="row g-3 mx-3">
-          <div className="col-md-6">
-            <label htmlFor="nome" className="form-label">
-              Nome
-            </label>
-            <input
-              className="form-control"
-              id="nome"
-              type="text"
-              name="nome"
-              value={newEmpreendedor.nome}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6">
-            <label htmlFor="sobrenome" className="form-label">
-              Sobrenome
-            </label>
-            <input
-              className="form-control"
-              id="sobrenome"
-              type="text"
-              name="sobrenome"
-              value={newEmpreendedor.sobrenome}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6">
-            <label htmlFor="email" className="form-label">
-              Email
-            </label>
-            <input
-              className="form-control"
-              id="email"
-              type="email"
-              name="email"
-              value={newEmpreendedor.email}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6">
-            <label htmlFor="telefone" className="form-label">
-              Telefone
-            </label>
-            <input
-              className="form-control"
-              id="telefone"
-              type="text"
-              name="telefone"
-              value={newEmpreendedor.telefone}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6">
-            <label htmlFor="dataNascimento" className="form-label">
-              Data de Nascimento
-            </label>
-            <input
-              className="form-control"
-              id="dataNascimento"
-              type="date"
-              name="dataNascimento"
-              value={newEmpreendedor.dataNascimento}
-              onChange={handleInputChange}
-            />
-          </div>
-
-           <div className="col-md-6">
-            <label htmlFor="nomeEmpresa" className="form-label">
-              Nome da Empresa
-            </label>
-            <input
-              className="form-control"
-              id="nomeEmpresa"
-              type="text"
-              name="nomeEmpresa"
-              value={newEmpreendedor.nomeEmpresa}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6">
-            <label htmlFor="cidade" className="form-label">
-              Cidade
-            </label>
-            <input
-              className="form-control"
-              id="cidade"
-              type="text"
-              name="cidade"
-              value={newEmpreendedor.cidade}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6">
-            <label htmlFor="estado" className="form-label">
-              Estado
-            </label>
-            <input
-              className="form-control"
-              id="estado"
-              type="text"
-              name="estado"
-              value={newEmpreendedor.estado}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6">
-            <label htmlFor="bairro" className="form-label">
-              Bairro
-            </label>
-            <input
-              className="form-control"
-              id="bairro"
-              type="text"
-              name="bairro"
-              value={newEmpreendedor.bairro}
-              onChange={handleInputChange}
-            />
-          </div>
-          {/* <div className="col-md-6">
-            <label htmlFor="mentoriasAdquiridas" className="form-label">
-              Mentorias Adquiridas
-            </label>
-            <input
-              className="form-control"
-              id="mentoriasAdquiridas"
-              type="text"
-              name="mentoriasAdquiridas"
-              value={newEmpreendedor.mentoriasAdquiridas}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6">
-            <label htmlFor="conteudosEmpreendedor" className="form-label">
-              Conteúdos Empreendedor
-            </label>
-            <input
-              className="form-control"
-              id="conteudosEmpreendedor"
-              type="text"
-              name="conteudosEmpreendedor"
-              value={newEmpreendedor.conteudosEmpreendedor}
-              onChange={handleInputChange}
-            />
-          </div> */}
-          <div className="col-md-6">
-            <label htmlFor="cep" className="form-label">
-              CEP
-            </label>
-            <input
-              className="form-control"
-              id="cep"
-              type="text"
-              name="cep"
-              value={newEmpreendedor.cep}
-              onChange={handleInputChange}
-            />
-          </div>
-
-          <div className="col-12">
-            <button className="btn btn-primary" onClick={handleAddEmpreendedor}>
-              Enviar
-            </button>
-          </div>
-        </form>
-
-        <div className="mt-3">
-          <Link href="/empreendedores" className="btn btn-secondary">
-            Voltar para a Lista de Empreendedores
-          </Link>
-        </div>
-      </div>
-    </>
+    <FormCreateUpdateDelete
+      title="Inserir Empreendedor"
+      linkHref="/admin/empreendedores"
+      // linkText="Voltar para a Lista de Empreendedores"
+      const
+      formData={[
+        {
+          id: "nome",
+          label: "Nome",
+          value: newEmpreendedor.nome,
+          type: "text",
+        },
+        {
+          id: "sobrenome",
+          label: "Sobrenome",
+          value: newEmpreendedor.sobrenome,
+          type: "text",
+        },
+        {
+          id: "email",
+          label: "E-mail",
+          value: newEmpreendedor.email,
+          type: "email",
+        },
+        {
+          id: "telefone",
+          label: "Telefone",
+          value: newEmpreendedor.telefone,
+          type: "text",
+        },
+        {
+          id: "dataNascimento",
+          label: "Data de Nascimento",
+          value: newEmpreendedor.dataNascimento,
+          type: "date",
+        },
+        {
+          id: "nomeEmpresa",
+          label: "Nome da Empresa",
+          value: newEmpreendedor.nomeEmpresa,
+          type: "text",
+        },
+        {
+          id: "cidade",
+          label: "Cidade",
+          value: newEmpreendedor.cidade,
+          type: "text",
+        },
+        {
+          id: "estado",
+          label: "Estado",
+          value: newEmpreendedor.estado,
+          type: "text",
+        },
+        {
+          id: "bairro",
+          label: "Bairro",
+          value: newEmpreendedor.bairro,
+          type: "text",
+        },
+        { id: "cep", label: "CEP", value: newEmpreendedor.cep, type: "text" },
+      ]}
+      handleInputChange={handleInputChange}
+      handleFormSubmitOrDelete={handleFormSubmit}
+      buttonText="Enviar"
+      backLink="/admin/empreendedores"
+    />
   );
 };
 
-export default AddEmpreendedor;
+export default CreateEmpreendedor;
