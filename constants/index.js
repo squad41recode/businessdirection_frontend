d127dfaa233bd7d01c5873f80d0b6faff0b4a4a5
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-01-24 21:22:36 -0300

    Create constants

---
+++ constants/index.js
@@ -0,0 +1,45 @@
+export const headerLinks = [
+  {
+    label: "Home",
+    route: "/",
+  },
+  {
+    label: "Ferramentas",
+    route: "/ferramentas",
+  },
+  {
+    label: "Sobre Nós",
+    route: "/sobre",
+  },
+  {
+    label: "Contato",
+    route: "/contato",
+  },
+  {
+    label: "Admin",
+    route: "/admin",
+  },
+  {
+    label: "Entrar",
+    route: "/entrar",
+  },
+  {
+    label: "Cadastrar",
+    route: "/cadastrar",
+  },
+];
+
+export const cursosHome = [
+  {
+    srcUrl: "/img/Finanças.jpg",
+    altText: "descricao da imagem",
+    titulo: "Finanças",
+    descricao: "A gestão financeira é o fator mais importante dentro da administração de uma empresa, possibilita a gestão de todos os recursos do negócio e facilita o fortalecimento e o crescimento do empreendimento."
+  },
+  {    
+    srcUrl: "/img/imagem.jpg",
+    altText: "descricao da imagem",
+    titulo: "titulo do curso",
+    descricao: "descricao do curso."
+  },
+];
--- pages/teste.js
+++ pages/teste.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { cursosHome } from "@/constants";
+import { Image } from "react-bootstrap";
 
 const teste = () => {
   return (
-    <div>teste</div>
-  )
-}
+    <div>
+      <h2>Cursos em Destaque:</h2>
+      <ul>
+        {cursosHome.map((curso) => (
+          <li key={curso.titulo}>
+            <Image
+            src={curso.srcUrl}
+            alt={curso.altText}
+            width={400}
+            height={200}
+            />
+            <h3>{curso.titulo}</h3>
+            <p>{curso.descricao}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default teste
+export default teste;
