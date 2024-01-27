8b5fa4b9d67752bf8262757fe58d0dd1925b2f8b
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-01-27 05:02:25 -0300

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -34,12 +34,51 @@ export const cursosHome = [
     srcUrl: "/img/Finanças.jpg",
     altText: "descricao da imagem",
     titulo: "Finanças",
-    descricao: "A gestão financeira é o fator mais importante dentro da administração de uma empresa, possibilita a gestão de todos os recursos do negócio e facilita o fortalecimento e o crescimento do empreendimento."
+    descricao:
+      "A gestão financeira é o fator mais importante dentro da administração de uma empresa, possibilita a gestão de todos os recursos do negócio e facilita o fortalecimento e o crescimento do empreendimento.",
   },
-  {    
+  {
     srcUrl: "/img/imagem.jpg",
     altText: "descricao da imagem",
     titulo: "titulo do curso",
-    descricao: "descricao do curso."
+    descricao: "descricao do curso.",
+  },
+];
+
+export const apiRoutes = [
+  {
+    nomeSing: "Empreendedor",
+    nomePlural: "Empreendedores",
+    rotaApi: "empreendedores",
+  },
+  {
+    nomeSing: "Mentor",
+    nomePlural: "Mentores",
+    rotaApi: "mentores",
+  },
+  {
+    nomeSing: "Mentoria disponível",
+    nomePlural: "Mentorias",
+    rotaApi: "mentorias-disponiveis",
+  },
+  {
+    nomeSing: "Conteudo Online",
+    nomePlural: "Conteúdos Online",
+    rotaApi: "conteudos-online",
+  },
+  {
+    nomeSing: "Modalidade de mentoria",
+    nomePlural: "Modalidades de mentorias",
+    rotaApi: "modalidades-mentorias",
+  },
+  {
+    nomeSing: "Conteúdo estudado",
+    nomePlural: "Conteúdos estudados",
+    rotaApi: "conteudos-online",
+  },
+  {
+    nomeSing: "Mentoria adquirida",
+    nomePlural: "Mentorias adquiridas",
+    rotaApi: "mentorias-adquiridas",
   },
 ];
