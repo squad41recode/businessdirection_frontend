6d2a7c028964f1b74bc59ebd9fc4763f0d96613b
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-01-27 05:01:53 -0300

    Update empreendedor.js

--- apiCalls/empreendedor.js
+++ apiCalls/empreendedor.js
@@ -1,5 +1,64 @@
 // apiCalls/empreendedor.js
-import { apiGet, apiPost, apiPut, apiDelete } from "../utils/apiCalls";
+import { apiGet, apiPost, apiPut, apiDelete } from "@/utils/apiCalls";
+
+export const EmpreendedorAtributtes = [
+  {
+    id: "id",
+    label: "ID",
+    type: "text",
+  },
+  {
+    id: "nome",
+    label: "Nome",
+    type: "text",
+  },
+  {
+    id: "sobrenome",
+    label: "Sobrenome",
+    type: "text",
+  },
+  {
+    id: "email",
+    label: "E-mail",
+    type: "email",
+  },
+  {
+    id: "telefone",
+    label: "Telefone",
+    type: "text",
+  },
+  {
+    id: "dataNascimento",
+    label: "Data de Nascimento",
+    type: "date",
+  },
+  {
+    id: "nomeEmpresa",
+    label: "Nome da Empresa",
+    type: "text",
+  },
+  {
+    id: "cidade",
+    label: "Cidade",
+    type: "text",
+  },
+  {
+    id: "estado",
+    label: "Estado",
+    type: "text",
+  },
+  {
+    id: "bairro",
+    label: "Bairro",
+    type: "text",
+  },
+  { 
+    id: "cep", 
+    label: "CEP", 
+    type: "text" 
+  },
+];
+
 
 export const getEmpreendedores = async () => {
   return apiGet("empreendedores");
