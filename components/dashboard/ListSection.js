8b86fce275e1e5d50bdbe0b61690347c26771141
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-01-27 05:02:12 -0300

    Update ListSection.js

--- components/dashboard/ListSection.js
+++ components/dashboard/ListSection.js
@@ -1,13 +1,30 @@
 import Link from "next/link";
 import style from "@/styles/Home.module.css";
+import DeleteModal from "@/components/dashboard/ModalDelete";
+import { apiDelete } from "@/utils/apiCalls";
 
-const ListSection = ({ title, linkHref, linkText, data, columns }) => {
+const ListSection = ({ title, endpoint, data, columns, entityName }) => {
+  // console.log("Data:", data);
+  // console.log("Columns:", columns);
+
+  const handleDeleteObject = async (id) => {
+    try {
+      // chama a função de exclusão da API passando o id
+      await apiDelete(`${endpoint}/${id}`);
+      console.log(`${entityName} excluído com sucesso!`);
+      //window.location.reload();
+    } catch (error) {
+      console.error(`Erro ao excluir ${entityName}:`, error);
+      alert(`Ocorreu um erro ao tentar deletar este ${entityName}`);
+    }
+  };
+
   return (
     <section>
-      <h1 className={style.h1}>{title}</h1>
+      <h1 className={style.h1}>{`Lista de ${title}`}</h1>
       <p>
-        <Link href={`/${linkHref}/create`} className="btn btn-add-admin">
-          {linkText}
+        <Link href={`/admin/${endpoint}/create`} className="btn btn-add-admin">
+          {`Inserir ${entityName}`}
         </Link>
       </p>
       <div className="d-flex flex-nowrap justify-content-between overflow-x-scroll">
@@ -15,7 +32,7 @@ const ListSection = ({ title, linkHref, linkText, data, columns }) => {
           <thead>
             <tr>
               {columns.map((column) => (
-                <th key={column}>{column}</th>
+                <th key={column.label}>{column.label}</th>
               ))}
               <th>Ações</th>
             </tr>
@@ -24,21 +41,29 @@ const ListSection = ({ title, linkHref, linkText, data, columns }) => {
             {data.map((element) => (
               <tr key={element.id} className={style.tabela}>
                 {columns.map((column) => (
-                  <td key={`${element.id}-${column}`}>{element[column]}</td>
+                  <td key={`${element.id}-${column.id}`}>
+                    {column.id
+                      ? getNestedValue(element, column.id)
+                      : "Campo não especificado"}
+                  </td>
                 ))}
                 <td>
-                  <Link
-                    href={`/${linkHref}/update/${element.id}`}
-                    className="btn btn-warning"
-                  >
-                    Editar
-                  </Link>
-                  <Link
-                    href={`/${linkHref}/delete/${element.id}`}
-                    className="btn btn-danger"
-                  >
-                    Excluir
-                  </Link>
+                  {endpoint !== "conteudos-estudados" &&
+                    endpoint !== "mentorias-adquiridas" && (
+                      <Link
+                        href={`/admin/${endpoint}/update/${element.id}`}
+                        className="btn btn-warning"
+                      >
+                        Editar
+                      </Link>
+                    )}
+                  <DeleteModal
+                    id={element.id}
+                    onDelete={() => handleDeleteObject(element.id)}
+                    entityName={entityName}
+                  />
+                  {/* <DeleteModal id={element.id} /> */}
+                  {/* onDeleteSuccess={handleSuccess} onDeleteError={handleError} */}
                 </td>
               </tr>
             ))}
@@ -47,6 +72,12 @@ const ListSection = ({ title, linkHref, linkText, data, columns }) => {
       </div>
     </section>
   );
+};
+
+// Função auxiliar para acessar campos aninhados
+const getNestedValue = (object, path) => {
+  const keys = path.split(".");
+  return keys.reduce((acc, key) => (acc && acc[key] ? acc[key] : ""), object);
 };
 
 export default ListSection;
