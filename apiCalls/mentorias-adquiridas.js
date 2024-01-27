b68c82348b7fe2760c80587c525650c953758d6b
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-01-27 05:01:58 -0300

    Create mentorias-adquiridas.js

---
+++ apiCalls/mentorias-adquiridas.js
@@ -0,0 +1,40 @@
+// MentoriaAdquiridaAtributtes
+export const MentoriaAdquiridaAtributtes = [
+    {
+      id: "id",
+      label: "ID",
+      type: "text",
+    },
+    {
+      id: "status",
+      label: "Status",
+      type: "text",
+    },
+    {
+      id: "encontrosFeitos",
+      label: "Encontros Feitos",
+      type: "text",
+    },
+    {
+      id: "faltas",
+      label: "Faltas",
+      type: "text",
+    },
+    {
+      id: "ativo",
+      label: "Ativo",
+      type: "text",
+    },
+  ];
+
+  export const createMentoriaAdquirida = (newMentoriaAdquirida) => {
+    return apiPost("mentorias-adquiridas", newMentoriaAdquirida);
+  };
+
+  export const updaMentoriaAdquirida = (id, updatedMentoriaAdquirida) => {
+   return apiPut(`mentorias-adquiridas/${id}`, updatedMentoriaAdquirida);
+  }; 
+
+  export const deleteMentoriaAdquirida = (id) => {
+    return apiDelete(`mentorias-adquiridas/${id}`);
+  };
