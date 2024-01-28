9192c8e02bcf9f003d14caf49880eeb90baac76a
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-01-28 03:26:29 -0300

    Update ferramentas

--- pages/ferramentas.js
+++ pages/ferramentas.js
@@ -1,72 +1,99 @@
-import React from 'react'
+import React from "react";
 
 const Ferramentas = () => {
-  
   return (
     <>
+      <main className="container">
+      <article className="container row mt-0">
+          <div className="col-12 col-lg-6 m-auto">
+          <h3>Nossas Planilhas</h3>
+          <p>
+            Lorem ipsum dolor sit amet. Qui autem temporibus ut rerum quaerat At
+            deleniti nemo sed dicta inventore. Aut illo illo sed illum quia quo
+            magnam illum. Vel tenetur iusto ut voluptatem molestiae vel voluptas
+            obcaecati est fugit officiis iste id omnis rerum sit
+            dolorem debitis aut molestias nulla! Et repellendus illo et galisum
+            quibusdam qui dolore ipsam et temporibus sunt est sunt nisi. Sed
+            quidem quibusdam eum galisum quasi et omnis repellat.
+          </p>
+          </div>
+          <div className=" col-12 col-lg-5" id=" ">
+            <div className="img-sn">
+              <img
+                src="../img/ferramentas/Data-extraction-amico.svg"
+                alt="img"
+                className="img-fluid"
+              />
+            </div>
+          </div>
+        </article>        
+        <section className="container-fluid cards-ferramentas text-center">
+          <div className="cards mt-3 ">
+            <div className="card ">
+                <h5 className="card-title">Simulador de empréstimo</h5>
+              <img
+                src="../img/sobrenos/services/Company-amico.svg"
+                className="card-img"
+                alt="..."
+              />
+              <div className="card-body">
+                {/* <p className="card-text">Breve descrição</p> */}
+              </div>
+            </div>
+            <div className="card  ">
+                <h5 className="card-title">Gestão financeira</h5>
+              <img
+                src="../img/sobrenos/services/Investment-data-amico.svg"
+                className="card-img"
+                alt="..."
+              />
+              <div className="card-body">
+                {/* <p className="card-text">Breve descrição</p> */}
+              </div>
+            </div>
+            <div className="card ">
+                <h5 className="card-title">Gestão de pessoas</h5>
+              <img
+                src="../img/sobrenos/services/office-amico.svg"
+                className="card-img"
+                alt="..."
+              />
+              <div className="card-body">
+                {/* <p className="card-text">Breve descrição</p> */}
+              </div>
+            </div>
+          </div>
+        </section>
+        <article className="container row mt-5">
+          <div className="col-12 col-lg-6 order-lg-last m-auto">
+            <h3>Nossos softwares</h3>
+            <p>
+              Lorem ipsum dolor sit amet. Qui autem temporibus ut rerum quaerat
+              At deleniti nemo sed dicta inventore. Aut illo illo sed illum quia
+              quo magnam illum. Vel tenetur iusto ut voluptatem molestiae vel
+              voluptas obcaecati est fugit quisquam est asperiores autem ut
+              tempora sint hic autem sequi. Est cumque delectus qui odit quae
+              eos necessitatibus ! Et repellendus illo et
+              galisum quibusdam qui dolore ipsam et temporibus sunt est sunt
+              nisi. Sed quidem quibusdam eum galisum quasi et omnis repellat.
+            </p>
+          </div>
+          <div className=" col-12 col-lg-5" id=" ">
+            <div className="img-sn">
+              <img
+                src="../img/ferramentas/Work-time-amico.svg"
+                alt="img"
+                className="img-fluid"
+              />
+            </div>
+          </div>
+        </article>
 
-    <main className="container">
-    <div className=" ">
-      {/* flex-column flex-sm-row justify-content-center  align-items-center */}
-      <ul className="row d-nav list-unstyled justify-content-center align-items-center">
-        {/* d-nav list-unstyled d-flex flex-wrap gap-4 justify-content-center  gap-3 */}
-        <li className="col-12 col-md-6 col-lg-3">
-          <a href="#" className="btn btn-primary d-block mb-3 me-md-3">
-            Simulador de empréstimo
-          </a>
-        </li>
-        <li className="col-12 col-md-6 col-lg-3 ">
-          <a href="#" className="btn btn-primary d-block mb-3 me-md-3">
-            Gestão financeira
-          </a>
-        </li>
-        <li className="col-12 col-md-6 col-lg-3 ">
-          <a href="#" className="btn btn-primary d-block mb-3 me-md-3">
-            Gestão de estoque
-          </a>
-        </li>
-        <li className="col-12 col-md-6 col-lg-3 ">
-          <a href="#" className="btn btn-primary d-block mb-3 me-md-3">
-            Gestão de pessoas
-          </a>
-        </li>
-      </ul>
-    </div>
-    <section>
-      <h3>Nossos softwares</h3>
-      <p>
-        Lorem ipsum dolor sit amet. Qui autem temporibus ut rerum quaerat At
-        deleniti nemo sed dicta inventore. Aut illo illo sed illum quia quo
-        magnam illum. Vel tenetur iusto ut voluptatem molestiae vel voluptas
-        obcaecati est fugit quisquam est asperiores autem ut tempora sint hic
-        autem sequi. Est cumque delectus qui odit quae eos necessitatibus sint
-        qui velit officiis et dolorem ipsum ut omnis eius. Ut officia fugiat aut
-        dolores nemo qui iste velit. Id ducimus internos hic commodi amet ab
-        officiis iste id omnis rerum sit dolorem debitis aut molestias nulla! Et
-        repellendus illo et galisum quibusdam qui dolore ipsam et temporibus
-        sunt est sunt nisi. Sed quidem quibusdam eum galisum quasi et omnis
-        repellat.
-      </p>
-    </section>
-    <section>
-      <h3>Nossas Planilhas</h3>
-      <p>
-        Lorem ipsum dolor sit amet. Qui autem temporibus ut rerum quaerat At
-        deleniti nemo sed dicta inventore. Aut illo illo sed illum quia quo
-        magnam illum. Vel tenetur iusto ut voluptatem molestiae vel voluptas
-        obcaecati est fugit quisquam est asperiores autem ut tempora sint hic
-        autem sequi. Est cumque delectus qui odit quae eos necessitatibus sint
-        qui velit officiis et dolorem ipsum ut omnis eius. Ut officia fugiat aut
-        dolores nemo qui iste velit. Id ducimus internos hic commodi amet ab
-        officiis iste id omnis rerum sit dolorem debitis aut molestias nulla! Et
-        repellendus illo et galisum quibusdam qui dolore ipsam et temporibus
-        sunt est sunt nisi. Sed quidem quibusdam eum galisum quasi et omnis
-        repellat.
-      </p>
-    </section>
-  </main>
+
+
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default Ferramentas
+export default Ferramentas;
