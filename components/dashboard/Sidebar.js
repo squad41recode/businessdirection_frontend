49facd0f30aa6606f53249fdf0f3b97dc49c9124
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-01-27 21:55:52 -0300

    Update Sidebar.js

--- components/dashboard/Sidebar.js
+++ components/dashboard/Sidebar.js
@@ -3,33 +3,36 @@ import Image from "next/image";
 import { apiRoutes } from "@/constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import styles from "@/styles/Dashboard.module.css"
+import style from "@/styles/Dashboard.module.css"
 
 const Sidebar = () => {
 const menuItems = apiRoutes;
-const pathname = usePathname()
+const pathname = usePathname();
+
   return (
-    <div className={`${styles.itemLink} ${styles.Sidebar} `}>
-      <div>
+    <aside className={` ${style.sidebar} `}>
+      <div className={` ${style.sidebarPerfil} `}>
         <Image
           className=""
-          src="/default-avatar.png"
-          alt=""
-          width="50"
-          height="50"
+          src="/img/default-profile-picture.png"
+          alt="foto perfil adm" 
+          width={65}
+          height={20}
         />
         <div className="">
           <span className="">Administrador</span>
         </div>
       </div>
-      <ul>
+      <ul className={` ${style.sidebarNavLink} `} >
         {menuItems.map((item) => (
-            <Link href={item.rotaApi} className={`${styles.itemLink} ${pathname === item.rotaApi && styles.active}`}>
+          <li key={item.rotaApi}>
+            <Link href={item.rotaApi}  className={`${style.itemLink} ${pathname === '/admin/' + item.rotaApi ? style.active : ''}`}>
               {item.nomePlural}
             </Link>
+          </li>
         ))}
       </ul>
-    </div>
+    </aside>
   );
 };
 
