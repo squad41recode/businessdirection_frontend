946de0c6b1d18639546724ee3e6e4c9943e1d012
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-01-28 03:25:40 -0300

    Update ListSection.js

--- components/dashboard/ListSection.js
+++ components/dashboard/ListSection.js
@@ -1,11 +1,27 @@
 import Link from "next/link";
-import style from "@/styles/Home.module.css";
+import style from "@/styles/Dashboard.module.css";
 import DeleteModal from "@/components/dashboard/ModalDelete";
 import { apiDelete } from "@/utils/apiCalls";
 
 const ListSection = ({ title, endpoint, data, columns, entityName }) => {
-  // console.log("Data:", data);
-  // console.log("Columns:", columns);
+  // Função para formatar o telefone
+  const formatPhoneNumber = (phoneNumber) => {
+    return phoneNumber.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+  };
+
+  // Função para formatar a data de nascimento
+  const formatDateOfBirth = (dateOfBirth) => {
+    const date = new Date(dateOfBirth);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
+
+  // Função para formatar o CEP
+  const formatCEP = (cep) => {
+    return cep.replace(/(\d{5})(\d{3})/, '$1-$2');
+  };
 
   const handleDeleteObject = async (id) => {
     try {
@@ -20,7 +36,7 @@ const ListSection = ({ title, endpoint, data, columns, entityName }) => {
   };
 
   return (
-    <section>
+    <section className="mx-auto">
       <h1 className={style.h1}>{`Lista de ${title}`}</h1>
       <p>
         <Link href={`/admin/${endpoint}/create`} className="btn btn-add-admin">
@@ -28,7 +44,7 @@ const ListSection = ({ title, endpoint, data, columns, entityName }) => {
         </Link>
       </p>
       <div className="d-flex flex-nowrap justify-content-between overflow-x-scroll">
-        <table className="table container tabela">
+        <table className={`${style.table} table table-responsive p-3 m-0`}>
           <thead>
             <tr>
               {columns.map((column) => (
@@ -38,16 +54,22 @@ const ListSection = ({ title, endpoint, data, columns, entityName }) => {
             </tr>
           </thead>
           <tbody>
-            {data.map((element) => (
-              <tr key={element.id} className={style.tabela}>
-                {columns.map((column) => (
-                  <td key={`${element.id}-${column.id}`}>
-                    {column.id
-                      ? getNestedValue(element, column.id)
-                      : "Campo não especificado"}
-                  </td>
-                ))}
-                <td>
+          {data.map((element) => (
+          <tr key={element.id} className={style.tabela}>
+            {columns.map((column) => (
+              <td key={`${element.id}-${column.id}`}>
+                {column.id
+                  ? column.id === 'telefone'
+                    ? formatPhoneNumber(getNestedValue(element, column.id))
+                    : column.id === 'dataNascimento' 
+                      ? formatDateOfBirth(getNestedValue(element, column.id))
+                      : column.id === 'cep'
+                        ? formatCEP(getNestedValue(element, column.id))
+                        : getNestedValue(element, column.id)
+                  : "Campo não especificado"}
+              </td>
+            ))}
+                <td className={style.tdAcoes}>
                   {endpoint !== "conteudos-estudados" &&
                     endpoint !== "mentorias-adquiridas" && (
                       <Link
