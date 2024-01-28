efdef2ff74c09e6c6a27610ac5668994d05d74b7
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-01-27 21:56:10 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,33 @@
 // pages/_app.js
 
-import Sidebar from '@/components/dashboard/Sidebar';
-import Layout from '@/components/layout';
+import DashboardLayout from '@/components/dashboard/DashboardLayout';
+import MainLayout from '@/components/MainLayout';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.pathname.startsWith('/admin')) {
+    }
+  }, [router.pathname]);
 
   return (
     <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      {router.pathname.startsWith('/admin') ? (
+        <DashboardLayout>
+          <Component {...pageProps} />
+        </DashboardLayout>
+      ) : (
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      )}
     </>
   );
 }
+
