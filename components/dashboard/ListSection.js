2b950d14307b6eb419bc5fe2a225a0e37e8a9e8e
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-01-28 21:24:27 -0300

    Update ListSection.js

--- components/dashboard/ListSection.js
+++ components/dashboard/ListSection.js
@@ -6,21 +6,21 @@ import { apiDelete } from "@/utils/apiCalls";
 const ListSection = ({ title, endpoint, data, columns, entityName }) => {
   // Função para formatar o telefone
   const formatPhoneNumber = (phoneNumber) => {
-    return phoneNumber.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+    return phoneNumber.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
   };
 
   // Função para formatar a data de nascimento
   const formatDateOfBirth = (dateOfBirth) => {
     const date = new Date(dateOfBirth);
-    const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
     const year = date.getFullYear();
     return `${day}/${month}/${year}`;
   };
 
   // Função para formatar o CEP
   const formatCEP = (cep) => {
-    return cep.replace(/(\d{5})(\d{3})/, '$1-$2');
+    return cep.replace(/(\d{5})(\d{3})/, "$1-$2");
   };
 
   const handleDeleteObject = async (id) => {
@@ -36,15 +36,21 @@ const ListSection = ({ title, endpoint, data, columns, entityName }) => {
   };
 
   return (
-    <section className="mx-auto">
+    <section className=" ">
+      <div className="">
       <h1 className={style.h1}>{`Lista de ${title}`}</h1>
       <p>
-        <Link href={`/admin/${endpoint}/create`} className="btn btn-add-admin">
+        <Link href={`/admin/${endpoint}/create`} className="btn btn-add-admin me-5">
           {`Inserir ${entityName}`}
         </Link>
       </p>
-      <div className="d-flex flex-nowrap justify-content-between overflow-x-scroll">
-        <table className={`${style.table} table table-responsive p-3 m-0`}>
+      </div>
+      <div className="d-flex flex-nowrap container justify-content-between overflow-scroll  ms-0">
+        {/*         
+      <div className="d-flex flex-nowrap justify-content-between overflow-x-scroll  ">
+      <table className=""> */}
+
+        <table className={`${style.table} table p-3 m-3`}>
           <thead>
             <tr>
               {columns.map((column) => (
@@ -54,35 +60,36 @@ const ListSection = ({ title, endpoint, data, columns, entityName }) => {
             </tr>
           </thead>
           <tbody>
-          {data.map((element) => (
-          <tr key={element.id} className={style.tabela}>
-            {columns.map((column) => (
-              <td key={`${element.id}-${column.id}`}>
-                {column.id
-                  ? column.id === 'telefone'
-                    ? formatPhoneNumber(getNestedValue(element, column.id))
-                    : column.id === 'dataNascimento' 
-                      ? formatDateOfBirth(getNestedValue(element, column.id))
-                      : column.id === 'cep'
+            {data.map((element) => (
+              <tr key={element.id} className={style.tabela}>
+                {columns.map((column) => (
+                  <td key={`${element.id}-${column.id}`}>
+                    {column.id
+                      ? column.id === "telefone"
+                        ? formatPhoneNumber(getNestedValue(element, column.id))
+                        : column.id === "dataNascimento"
+                        ? formatDateOfBirth(getNestedValue(element, column.id))
+                        : column.id === "cep"
                         ? formatCEP(getNestedValue(element, column.id))
                         : getNestedValue(element, column.id)
-                  : "Campo não especificado"}
-              </td>
-            ))}
+                      : "Campo não especificado"}
+                  </td>
+                ))}
                 <td className={style.tdAcoes}>
                   {endpoint !== "conteudos-estudados" &&
                     endpoint !== "mentorias-adquiridas" && (
                       <Link
                         href={`/admin/${endpoint}/update/${element.id}`}
-                        className="btn btn-warning btn-edit-admin"
+                        className="btn btn-warning btn-edit-admin "
                       >
-                        Editar
+                        <i className="bi-pen"></i>
                       </Link>
                     )}
                   <DeleteModal
                     id={element.id}
                     onDelete={() => handleDeleteObject(element.id)}
-                    entityName={entityName} className="btn-del-admin"
+                    entityName={entityName}
+                    className="btn-del-admin"
                   />
                   {/* <DeleteModal id={element.id} /> */}
                   {/* onDeleteSuccess={handleSuccess} onDeleteError={handleError} */}
